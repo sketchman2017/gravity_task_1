fd5c6db980c9e34e2ba3509ca40734d7177dc5c2
sketchman2017 <sketchman2017@gmail.com>
2016-10-11 19:44:45 +0300

    2nd task added

--- task_1.py
+++ task_1.py
@@ -49,4 +49,74 @@ def tests():
     value = get_main_frequency([])
     print '[], result', value
 
-tests()
+# Найти все минимумы и максимумы
+
+def max_profit(a):
+    pass
+
+def task_2(a, b, m):
+    if m == 0:
+        return 0
+
+    k = 0
+    # Увеличиваем i до первого делящегося числа от а сверху
+    i = a
+    while i <= b:
+        if i % m == 0:
+            k = k + 1
+            break
+        i = i + 1
+    if i >= b:
+        return k
+
+    # Увеличиваем j до первого делящегося числа
+    j = b
+    while j > a:
+        if j % m == 0 and j != i:
+            k = k + 1
+            break
+        j = j - 1
+
+    if j <= a:
+        return k
+
+    return (j - i) / m + k - 1
+
+def tests_2():
+    # Тесты
+
+    count = task_2(6, 11, 2)
+    print 'A=6, B=11, M=2. result =', count
+
+    count = task_2(3, 17, 3)
+    print 'A=3, B=17, M=3. result =', count
+
+    count = task_2(10, 13, 97)
+    print 'A=10, B=13, M=97. result =', count
+
+    count = task_2(10, 97, 13)
+    print 'A=10, B=97, M=13. result =', count
+
+    count = task_2(0, 2000000000, 2)
+    print 'A=0, B=2000000000, M=2. result =', count
+
+    count = task_2(0, 10, 0)
+    print 'A=0, B=10, M=0. result =', count
+
+    count = task_2(0, 10, 1)
+    print 'A=0, B=10, M=1. result =', count
+
+    count = task_2(5, 10, 5)
+    print 'A=5, B=10, M=5. result =', count
+
+    count = task_2(5, 5, 5)
+    print 'A=5, B=5, M=5. result =', count
+
+    count = task_2(3, 90, 3)
+    print 'A=3, B=90, M=3. result =', count
+
+    count = task_2(3, -3, 3)
+    print 'A=3, B=-3, M=3. result =', count
+
+    count = task_2(-3, 9, 3)
+    print 'A=-3, B=9, M=3. result =', count
